0410a9b05bb2ddb88fa0200c861cb984633c28bf
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-04-21 19:44:26 -0700

    Add populate functionality that puts tasks in tables based on their completeness

---
+++ completeness.js
@@ -0,0 +1,62 @@
+/**
+ * Fetch data from json file and add to table
+ * 
+ * @param {String} file
+ */
+const fetchJson = (file) => {
+    fetch(file)
+        .then(data => data.json())
+        .then(json => {
+            populateTable(json);
+        })
+        .catch((err) => console.error(err));
+}
+
+/**
+ * Adds all the tasks to their corresponding table
+ * 
+ * @param {JSON} taskList
+ */
+const populateTable = (taskList) => {
+    // go through each task and create a row
+    taskList.forEach((task) => {
+        // gets table that the task belongs to
+        const tableId = (task.completed ? "" : "in") + "complete-table";
+        const table = document.getElementById(tableId);
+
+        const row = createRow(task);
+        table.appendChild(row);
+    })
+}
+
+/**
+ * Creates a row with the input task's information
+ * 
+ * @param {Object} task
+ * @returns row element to be added to the table
+ */
+const createRow = (task) => {
+    // create a row element
+    const row = document.createElement('tr');
+
+    // title of task
+    const title = document.createElement('td');
+    title.textContent = task.title;
+
+    // description of task
+    const description = document.createElement('td');
+    description.textContent = task.descriptions;
+
+    // date of task
+    const date = document.createElement('td');
+    date.textContent = new Date(task.due_date).toLocaleString();
+
+    // append to row
+    row.appendChild(title);
+    row.appendChild(description);
+    row.appendChild(date);
+
+    return row;
+}
+
+fetchJson('./tasks.json');
