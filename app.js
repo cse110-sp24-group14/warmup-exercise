711e80aa765aedc88fd45f51105ed95d7216341f
Advaith Modali <advaith0331@gmail.com>
2024-04-25 23:22:57 -0700

    removed comments, extraneous aspects, switched order of Due Date and Task sections

--- app.js
+++ app.js
@@ -3,13 +3,6 @@
  * Sortes the tasks by due date
  * returns an array of JSON objects sorted by due date
  * 
- return data type example: [{},{},{},{},{},{}]
- 0: {id_val: 1, title_val: '110 homework 1', descriptions_val: 'Refer to Canvas', date_val: Tue Apr 09 2024 03:03:00 GMT-0700 (Pacific Daylight Time), completed_val: true}
- 1: {id_val: 2, title_val: '110 homework 2', descriptions_val: 'Refer to Canvas', date_val: Tue Apr 09 2024 13:43:00 GMT-0700 (Pacific Daylight Time), completed_val: false}
- 2: {id_val: 3, title_val: '110 homework 3', descriptions_val: 'Refer to Canvas', date_val: Wed Apr 10 2024 03:03:00 GMT-0700 (Pacific Daylight Time), completed_val: true}
- 3: {id_val: 5, title_val: '110 homework 5', descriptions_val: 'Refer to Canvas', date_val: Sat Apr 13 2024 03:03:00 GMT-0700 (Pacific Daylight Time), completed_val: true}
- 4: {id_val: 6, title_val: '110 homework 6', descriptions_val: 'Refer to Canvas', date_val: Sat Apr 13 2024 13:43:00 GMT-0700 (Pacific Daylight Time), completed_val: false}
- 5: {id_val: 4, title_val: '110 homework 4', descriptions_val: 'Refer to Canvas', date_val: Mon Apr 15 2024 13:43:00 GMT-0700 (Pacific Daylight Time), completed_val: false}
  */
 function populate_table_by_date() {
   const parsed_dates = [];
@@ -26,7 +19,6 @@ function populate_table_by_date() {
         });
       });
       parsed_dates.sort((a, b) => a.date_val - b.date_val);
-      console.log("sorted parsed dates", parsed_dates);
     });
     return parsed_dates;
 }
