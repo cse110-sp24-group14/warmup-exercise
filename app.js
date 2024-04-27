e6912bc01f849a206a896ab3097953ab1100912a
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2024-04-26 22:09:33 -0700

    Brenton/responsive header (#29)
    * Fix mobile view for header
    
    * Simplify files
    
    * Remove unnecessary header

--- app.js
+++ app.js
@@ -46,3 +46,86 @@ function showView(radio) {
 function openContactLink() {
   window.open('https://github.com/cse110-sp24-group14/cse110-sp24-group14/blob/main/admin/team.md', '_blank');
 }
+
+/**
+ * Fetch data from json file and add to table
+ * 
+ * @param {String} file
+ */
+const fetchJson = (file) => {
+  fetch(file)
+      .then(data => data.json())
+      .then(json => {
+          populateTable(json);
+      })
+      .catch((err) => console.error(err));
+}
+
+/**
+* Adds all the tasks to their corresponding table
+* 
+* @param {JSON} taskList
+*/
+const populateTable = (taskList) => {
+  // go through each task and create a row
+
+  const taskCount = {"incomplete": 0, "complete": 0};
+
+  taskList.forEach((task) => {
+      // gets table that the task belongs to
+      const completeBool = task.completed ? "complete" : "incomplete";
+
+      const tableId = `${completeBool}-table`;
+      const table = document.getElementById(tableId);
+
+      const row = createRow(task, task.completed);
+      table.appendChild(row);
+
+      // count number of each task
+      taskCount[completeBool] += 1;
+  })
+
+  // update the count of both types of tasks
+  for (let count in taskCount) {
+      const title = document.getElementById(`${count}-title`);
+      title.textContent += ` (${taskCount[count]})`;
+  }
+}
+
+/**
+* Creates a row with the input task's information
+* 
+* @param {Object} task
+* @returns row element to be added to the table
+*/
+const createRow = (task, completed) => {
+  // create a row element
+  const row = document.createElement('tr');
+
+  // date of task
+  const date = document.createElement('td');
+  // get in format MM/dd
+  date.textContent = new Date(task.due_date).toLocaleString(undefined, {
+      month: "numeric", day: "numeric"
+  })
+
+  // description of task
+  const description = document.createElement('td');
+  description.textContent = new Date(task.due_date).toLocaleString(undefined, {
+      month: "numeric", day: "numeric"
+  }) + " | " + task.description;
+
+  // if completed, strikethrough
+  if (completed) {
+      description.style.textDecoration = "line-through";
+  }
+
+  description.className = "c-description"
+  date.className = "c-date"
+
+  row.appendChild(description);
+
+  return row;
+}
+
+fetchJson('./tasks.json');
